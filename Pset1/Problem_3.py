d64124ac2ee3d7ef070d47f8ef1a082565cd5074
nbarendes <nbarendes@gmail.com>
2021-03-21 12:55:59 +0100

    longest substring in alphabetical order

---
+++ Pset1/Problem_3.py
@@ -0,0 +1,17 @@
+"""
+prints the longest substring of s in which the letters occur in alphabetical order
+"""
+maxlength = 0
+current = s[0]
+longest = s[0]
+
+for i in range(len(s) - 1):
+	if s[i+1] >= s[i]:
+		current += s[i+1]
+		if len(current) > maxlength:
+			maxlength = len(current)
+			longest = current
+	else:
+		current = s[i+1]
+	i += 1
+print("The longest substring in alphabetical order is:" + longest) 
